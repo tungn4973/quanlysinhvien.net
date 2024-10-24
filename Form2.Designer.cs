fe1fb2f2f79a5daf4255858fede310c87a5bb536
tungn4973 <tungn4973@gmail.com>
2024-10-24 16:33:05 +0700

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvStudents = new DataGridView();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            txtStudentId = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtStudentName = new TextBox();
+            txtStudentClass = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvStudents).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvStudents
+            // 
+            dgvStudents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvStudents.Location = new Point(26, 45);
+            dgvStudents.Name = "dgvStudents";
+            dgvStudents.RowHeadersWidth = 51;
+            dgvStudents.Size = new Size(550, 244);
+            dgvStudents.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(111, 562);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Thêm ";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(282, 562);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(94, 29);
+            btnEdit.TabIndex = 2;
+            btnEdit.Text = "Sửa";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(482, 562);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Xóa";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // txtStudentId
+            // 
+            txtStudentId.Location = new Point(208, 309);
+            txtStudentId.Name = "txtStudentId";
+            txtStudentId.Size = new Size(266, 27);
+            txtStudentId.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 323);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Mã sinh viên :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(60, 377);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Tên :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(59, 443);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Lớp :";
+            // 
+            // txtStudentName
+            // 
+            txtStudentName.Location = new Point(208, 374);
+            txtStudentName.Name = "txtStudentName";
+            txtStudentName.Size = new Size(266, 27);
+            txtStudentName.TabIndex = 9;
+            // 
+            // txtStudentClass
+            // 
+            txtStudentClass.Location = new Point(208, 443);
+            txtStudentClass.Name = "txtStudentClass";
+            txtStudentClass.Size = new Size(266, 27);
+            txtStudentClass.TabIndex = 10;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(641, 628);
+            Controls.Add(txtStudentClass);
+            Controls.Add(txtStudentName);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtStudentId);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Controls.Add(dgvStudents);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvStudents).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvStudents;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private TextBox txtStudentId;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtStudentName;
+        private TextBox txtStudentClass;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Student
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Class { get; set; }
+        }
+        // Danh sách sinh viên
+        private List<Student> studentList = new List<Student>();
+
+        // Tải dữ liệu mẫu
+        private void LoadData()
+        {
+            studentList.Add(new Student { Id = "SV001", Name = "Nguyen Van A", Class = "Lop 12A1" });
+            studentList.Add(new Student { Id = "SV002", Name = "Le Thi B", Class = "Lop 12A2" });
+
+            dgvStudents.DataSource = null;
+            dgvStudents.DataSource = studentList;
+        }
+
+        // Nút "Thêm"
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Student newStudent = new Student
+            {
+                Id = txtStudentId.Text,
+                Name = txtStudentName.Text,
+                Class = txtStudentClass.Text
+            };
+
+            studentList.Add(newStudent);
+            dgvStudents.DataSource = null;
+            dgvStudents.DataSource = studentList;
+        }
+
+        // Nút "Sửa"
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.SelectedRows.Count > 0)
+            {
+                Student selectedStudent = dgvStudents.SelectedRows[0].DataBoundItem as Student;
+
+                selectedStudent.Id = txtStudentId.Text;
+                selectedStudent.Name = txtStudentName.Text;
+                selectedStudent.Class = txtStudentClass.Text;
+
+                dgvStudents.DataSource = null;
+                dgvStudents.DataSource = studentList;
+            }
+        }
+
+        // Nút "Xóa"
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.SelectedRows.Count > 0)
+            {
+                Student selectedStudent = dgvStudents.SelectedRows[0].DataBoundItem as Student;
+                studentList.Remove(selectedStudent);
+
+                dgvStudents.DataSource = null;
+                dgvStudents.DataSource = studentList;
+            }
+        }
+
+        // Sự kiện chọn hàng
+        private void dgvStudents_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvStudents.SelectedRows.Count > 0)
+            {
+                Student selectedStudent = dgvStudents.SelectedRows[0].DataBoundItem as Student;
+                if (selectedStudent != null)
+                {
+                    txtStudentId.Text = selectedStudent.Id;
+                    txtStudentName.Text = selectedStudent.Name;
+                    txtStudentClass.Text = selectedStudent.Class;
+                }
+            }
+        }
+
+    }
+}
